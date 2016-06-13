2c22269d1e7048ff67a5b8b4a4ec0b392015683b
John Washam <john@readyprompt.com>
2016-06-13 09:55:48 -0700

    Adding linked list project.

---
+++ linked_lists/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linked_list.h"
+#include "linked_list.c"
+
+void run_example();
+
+// Implements a linked list and doubly-linked list
+
+int main(int argc, char* argv[]) {
+  //run_all_tests();
+  run_example();
+
+  return EXIT_SUCCESS;
+}
+
+void run_example() {
+
+}
