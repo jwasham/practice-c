2398bd923acf6d2332918256157e69c78b857286
John Washam <john@readyprompt.com>
2016-07-18 22:14:27 -0700

    Added more variety to input numbers.

--- merge_sort/main.c
+++ merge_sort/main.c
@@ -1,14 +1,15 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include "merge_sort.c"
 #include "test_merge_sort.c"
 
 int main(int argc, char* argv[]) {
   int num_count = 30;
-  int numbers[] = {5432, 989, 7510, 3,   -93, 8919, 542, 123,  597, 42,
-                   7506, 184, 184, 2409, 45,  824,  4,   2650, 9,   662,
-                   3928, 170, 5358, 395, 842, 7697, 110, 14,   99,  221};
+  int numbers[] = {325432, 989,   547510, 3,   -93,  189019, 5042,  123,
+                   597,    42,    7506,   184, 184,  2409,   45,    824,
+                   4,      -2650, 9,      662, 3928, -170,   45358, 395,
+                   842,    7697,  110,    14,  99,   221};
 
   printf("Sorting %d numbers...\n\n", num_count);
 
