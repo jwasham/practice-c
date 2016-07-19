905adb14400c3b92354a3099ba88787643860881
John Washam <john@readyprompt.com>
2016-07-19 13:20:24 -0700

    My flavor of merge sort.

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,33 +1,36 @@
-void merge(int a[], int low, int mid, int high) {
-  // temporary array for holding merged values
+void merge(int numbers[], int low, int mid, int high) {
+  // temp array for holding sorted
   int b[high - low - 1];
   int i = low;
   int j = mid + 1;
   int k = 0;
 
+  // merge items from list in order
   while (i <= mid && j <= high) {
-    if (a[i] <= a[j])
-      b[k++] = a[i++];
-    else
-      b[k++] = a[j++];
+    if (numbers[i] <= numbers[j]) {
+      b[k++] = numbers[i++];
+    } else {
+      b[k++] = numbers[j++];
+    }
   }
 
-  while (i <= mid) b[k++] = a[i++];
-
-  while (j <= high) b[k++] = a[j++];
+  // copy the remaining items to tmp array
+  while (i <= mid) b[k++] = numbers[i++];
+  while (j <= high) b[k++] = numbers[j++];
 
   --k;
   while (k >= 0) {
-    a[low + k] = b[k];
+    numbers[low + k] = b[k];
     --k;
   }
 }
 
-void merge_sort(int a[], int low, int high) {
+void merge_sort(int numbers[], int low, int high) {
   if (low < high) {
     int mid = (low + high) / 2;
-    merge_sort(a, low, mid);
-    merge_sort(a, mid + 1, high);
-    merge(a, low, mid, high);
+
+    merge_sort(numbers, low, mid);
+    merge_sort(numbers, mid + 1, high);
+    merge(numbers, low, mid, high);
   }
 }
--- merge_sort/merge_sort.h
+++ merge_sort/merge_sort.h
@@ -3,7 +3,7 @@
 
 // Overwrites the original array with merged values.
 void merge(int a[], int low, int mid, int high);
-// Sorts the given array, from index low to high
+// Sorts the slice of the given array from index low to index high
 void merge_sort(int a[], int low, int high);
 
 #endif  // PROJECT_MERGE_SORT_H
