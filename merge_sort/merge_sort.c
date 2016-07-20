311c3596a1b565340d70343968b4a27a999caf40
John Washam <john@readyprompt.com>
2016-07-19 17:53:13 -0700

    Added to comment.

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,5 +1,5 @@
 void merge(int numbers[], int low, int mid, int high) {
-  // temp array for holding sorted
+  // temp array for holding sorted items
   int b[high - low - 1];
   int i = low;
   int j = mid + 1;
