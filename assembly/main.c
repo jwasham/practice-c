b505bc7e9b3e77f034719c6ee2ba1c126f8770a3
John Washam <john@readyprompt.com>
2016-08-29 10:19:22 -0700

    Looking at assembly code.

---
+++ assembly/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Just a simple program so I can inspect the assembly code.
+ */
+
+int main(int argc, char *argv[]) {
+
+  printf("I like the number %d.\n", 12345);
+
+  return 0xf00d;
+}
