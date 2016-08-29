6ebfb28726ed2883c7c8984bc684184bd928cc64
John Washam <john@readyprompt.com>
2016-08-29 10:21:27 -0700

    Adding assembly code.

---
+++ assembly/main.s
@@ -0,0 +1,30 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 11
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	leaq	L_.str(%rip), %rdi
+	movl	$12345, %esi            ## imm = 0x3039
+	xorl	%eax, %eax
+	callq	_printf
+	movl	$61453, %eax            ## imm = 0xF00D
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"I like the number %d.\n"
+
+
+.subsections_via_symbols
